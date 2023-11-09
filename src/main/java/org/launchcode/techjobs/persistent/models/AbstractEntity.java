dcb96dd6e20cc95aac9861a8242b498db9e40e66
Kevin <cfphpl@gmail.com>
2023-11-09 13:46:10 -0600

    task 2.1

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -4,16 +4,32 @@ package org.launchcode.techjobs.persistent.models;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
 import java.util.Objects;
 
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Name is required")
+    @Size(max = 255, message = "Name must be less than 255 characters")
     private String name;
+
+    // Constructors
+
+    public AbstractEntity() {
+        // Default constructor
+    }
+
+    public AbstractEntity(String name) {
+        this.name = name;
+    }
+
+    // Getters and setters
 
     public int getId() {
         return id;
@@ -27,10 +43,7 @@ public abstract class AbstractEntity {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return name;
-    }
+    // Other methods
 
     @Override
     public boolean equals(Object o) {
@@ -44,5 +57,4 @@ public abstract class AbstractEntity {
     public int hashCode() {
         return Objects.hash(id);
     }
-
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,30 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is required")
+    @Size(max = 255, message = "Location must be less than 255 characters")
+    private String location;
+
+    public Employer() {
+    }
+
+    public Employer(String name, String location) {
+        super(name);
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
+
