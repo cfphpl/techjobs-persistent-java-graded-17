828ed8d7df0a726075756c6527768688f73701b8
Kevin <cfphpl@gmail.com>
2023-11-09 13:56:22 -0600

    task 2.2

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Description is required")
+    @Size(max = 500, message = "Description must be less than 500 characters")
+    private String description;
+
+    public Skill() {
+
+    }
+
+    public Skill(String name, String description) {
+        super(name);
+        this.description = description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
