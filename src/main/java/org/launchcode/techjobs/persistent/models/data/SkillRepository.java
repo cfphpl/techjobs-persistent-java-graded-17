0964d1daf20a00d0050275b02339ed2f58e99a8c
Kevin <cfphpl@gmail.com>
2023-11-09 18:26:40 -0600

    comments

--- src/main/java/org/launchcode/techjobs/persistent/models/data/SkillRepository.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/SkillRepository.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.persistent.models.data;
 
+//Mom's surgery 16th Anderson Hospital. Arrive at noon. Surgery at 2pm
+// KEVIN delete this once you put it in the calendar!
 import org.launchcode.techjobs.persistent.models.Skill;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
